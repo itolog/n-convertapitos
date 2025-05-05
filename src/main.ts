789c32b1d6db394f0210e659e0a881f75394676e
itolog <paukanlove@gmail.com>
2025-05-05 12:00:17 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
+
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+
+const PORT = process.env.PORT ?? 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(PORT, () => {
+    Logger.log(`App listen on http://localhost/${PORT}`);
+  });
+}
+
+bootstrap().catch(() => {
+  Logger.error("Something went wrong.");
+});
