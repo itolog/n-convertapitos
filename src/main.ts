b1ed37bfa9ef6dd6254435c0215685695f36bc05
itolog <paukanlove@gmail.com>
2025-05-05 12:04:21 +0300

    add base config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import { Logger } from "@nestjs/common";
-
+import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+
+import { AppModule } from "./app.module";
 
 const PORT = process.env.PORT ?? 3000;
 
